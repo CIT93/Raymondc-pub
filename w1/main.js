8f732d36dd8c40d3f5747889a36d7a6203422c20
cheray91 <cheray91@icloud.com>
2025-01-24 19:24:37 +0000

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,25 @@
-console.log("week 1 - Hello World");
+console.log("Hello from inside the main.js file");
+
+//let myVar;
+
+const myVar = "Raymond";
+const myValType = typeof(myVar);
+
+console.log("myValType" + myValType);
+console.log(`myValType ${myValType}`);
+
+if(myValType === "number") {
+  console.log(`this 12 will run`);  
+} else {
+    console.log(`this 14 will not run`);
+}
+
+function runNow() {
+    if(myValType === "number") {
+        console.log(`this 12 will run`);  
+      } else {
+          console.log(`this 14 will not run`);
+      }
+}
+runNow()
+runNow()
