74ed7fcbf64abd2606b87a36392c3f7e8be00331
cheray91 <cheray91@icloud.com>
2025-01-30 20:51:37 +0000

    working code

--- w3/mian.js
+++ w3/mian.js
@@ -1,25 +1,46 @@
 function determineHouseHoldpts(NumberInhousehold) {
 console.log("inside the function")
 if (NumberInhousehold === 1){
-    CarbonFootprintPoints = CarbonFootprintPoints + 14;
+    carbonFootprintPoints = carbonFootprintPoints + 14;
 } else if(NumberInhousehold === 2) {
-    CarbonFootprintPoints = CarbonFootprintPoints + 12;
+    carbonFootprintPoints = carbonFootprintPoints + 12;
 } else if(NumberInhousehold === 3){
-    CarbonFootprintPoints = CarbonFootprintPoints + 10;  
+    carbonFootprintPoints = carbonFootprintPoints + 10;  
 } else if(NumberInhousehold === 4){
-    CarbonFootprintPoints = CarbonFootprintPoints + 8;
+    carbonFootprintPoints = carbonFootprintPoints + 8;
 } else if(NumberInhousehold === 5) {
-    CarbonFootprintPoints = CarbonFootprintPoints + 6;
+    carbonFootprintPoints = carbonFootprintPoints + 6;
 } else if (NumberInhousehold === 6){
-    CarbonFootprintPoints = CarbonFootprintPoints + 4;
+    carbonFootprintPoints = carbonFootprintPoints + 4;
 } else if(NumberInhousehold > 6) {
-    CarbonFootprintPoints = CarbonFootprintPoints + 2;
+    carbonFootprintPoints = carbonFootprintPoints + 2;
 }
-    console.log(`Based on the number of number of the household of ${NumberInhousehold} the points would be ${CarbonFootprintPoints}.`);
+    console.log(`Based on the number of number of the household of ${NumberInhousehold} the points would be ${carbonFootprintPoints}.`);
 }
-let CarbonFootprintPoints = 0;
-const NumberInhousehold = 3;
+let carbonFootprintPoints = 0;
+const numberInhousehold = 3;
 
 //space
 determineHouseHoldpts(3)
 determineHouseHoldpts(4)
+//If you have a large house, then add 10 points to your score.
+//If you have a medium-sized house, then add 7 points.
+//If you have a small house, then add 4 points.
+//If you live in an apartment, then add 2 points
+
+function myHouseholdSizecfp(householdsize) {
+    console.log("inside the householdsize function");
+    if(householdsize === "large"){
+        carbonFootprintPoints = carbonFootprintPoints + 10;
+    } else if(householdsize === "medium"){
+        carbonFootprintPoints = carbonFootprintPoints + 7;  
+    } else if(householdsize === "small"){
+        carbonFootprintPoints = carbonFootprintPoints + 4;
+    }else if(householdsize === "Apartment"){
+        carbonFootprintPoints = carbonFootprintPoints + 2;
+    }
+    console.log(`Based on ${householdsize} household size the points would be ${carbonFootprintPoints}.`);    
+}
+ 
+ myHouseholdSizecfp("large")
+ 
