947ea57f16bf44c2cc3b5695b19b4f5199beb965
cheray91 <cheray91@icloud.com>
2025-02-15 21:19:13 +0000

    list of movies

--- w5/main.js
+++ w5/main.js
@@ -1,4 +1,4 @@
-const cfpData = [];
+/*const cfpData = [];
 function determineHouseSizePts(size) {
     let houseSizePoints = 0;
     if(size === "large") {
@@ -31,64 +31,87 @@ function determineHouseSizePts(size) {
       houseHoldPoints = 2;
     }
      return houseHoldPoints;
-  }
-  function displayOutput(){
-    for (obj of cfpData){
-  console.log(obj);
-  const output = document.getElementById("output");
-  const newH2 = document.createElement("h2");
-  newH2.textContent = `Carbon Footprint total is ${obj.cfpTotal}`;
-  const newH3 = document.createElement("h3");
-  newH3.textContent = `Based on Number in Household and Size of Home`
-  const newP = document.createElement("p");
-    newP.textContent = `This number is based on the number in household of ${obj.houseM} (score: ${obj.houseMPTS}) `;
-    newP.textContent += `and a ${obj.houseS} size of home (score: ${obj.houseSPTS})`;
-    
-  output.appendChild(newH2);
-  output.appendChild(newH3);
-  output.appendChild(newP);
-    }
-  }
+  }*/
+/*function displayOutput(){
+  for (obj of cfpData){
+console.log(obj);
+const output = document.getElementById("output");
+const newH2 = document.createElement("h2");
+newH2.textContent = `Carbon Footprint total is ${obj.cfpTotal}`;
+const newH3 = document.createElement("h3");
+newH3.textContent = `Based on Number in Household and Size of Home`
+const newP = document.createElement("p");
+  newP.textContent = `This number is based on the number in household of ${obj.houseM} (score: ${obj.houseMPTS}) `;
+  newP.textContent += `and a ${obj.houseS} size of home (score: ${obj.houseSPTS})`;
   
-  
-  function start(houseHoldMembers, houseSize) {
-  const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
-  const houseSizePts = determineHouseSizePts(houseSize);
-  const total = houseHoldPTS + houseSizePts;
-   
+output.appendChild(newH2);
+output.appendChild(newH3);
+output.appendChild(newP);
+  }
+}
+ 
+ 
+function start(houseHoldMembers, houseSize) {
+const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
+const houseSizePts = determineHouseSizePts(houseSize);
+const total = houseHoldPTS + houseSizePts;
+ 
 
-  cfpData.push({
-    houseM: houseHoldMembers,
-    houseS: houseSize,
-    houseMPTS: houseHoldPTS,
-    houseSPTS: houseSizePts,
-    cfpTotal: total
-  });
+cfpData.push({
+  houseM: houseHoldMembers,
+  houseS: houseSize,
+  houseMPTS: houseHoldPTS,
+  houseSPTS: houseSizePts,
+  cfpTotal: total
+});
+ 
+}*/
+
+
+
+//function displayOutput(myCfpObj) {
+//for(myCfpObj) {
+//const output = document.getElementById("Output");
+//const newP = document.createElement("p");
+//const newH2 = document.createElement("h2");
+//newH2.textContent = `Carbon Footprint total is ${arr[4]}`;
+//output.appendChild(newH2);
+//const newH3 = document.createElement("h3");
+//newH3.textContent = `Based on Number in Household and Size of Home`;
+//newP.textContent = `This number is based on the number of housedhold of ${arr[0]} (score: ${arr[3]})`;
+//newP.textContent += `and a ${arr[1]} size of home (score: ${arr[2]})`;
+//output.appendChild(newH2);
+//output.appendChild(newH3);
+//output.appendChild(newP);
+//}
+//}
+
+/* start(1, "apt");
+ start(2, "apt");
+ start(3, "apt");
+ start(4, "apt");
+ start(5, "apt");
+displayOutput();*/
+
+// an array of of onjects
+
+
+const moviesArr = [
+  { title: "Oppenheimer", year: 2023, rating: "8.3/10" },
+  { title: "Past Lives", year: 2023, rating: "7.8/10" },
+  { title: "Death on the Nile", year: 2022, rating: "8.2/10" },
+  { title: "Uncharted", year: 2022, rating: "8.5/10" },
+];
+
+function displayOutMovies() {
+  const output = document.getElementById("output");
   
+  for (const obj of moviesArr) {
+    console.log(obj);
+    const newH1 = document.createElement("h1");
+    newH1.textContent = `${obj.title} (released in ${obj.year}) has a rating of ${obj.rating}`;
+    output.appendChild(newH1);
   }
-  
-   
-  
-  //function displayOutput(myCfpObj) {
-  //for(myCfpObj) {
-  //const output = document.getElementById("Output");
-  //const newP = document.createElement("p");
-  //const newH2 = document.createElement("h2");
-  //newH2.textContent = `Carbon Footprint total is ${arr[4]}`;
-  //output.appendChild(newH2);
-  //const newH3 = document.createElement("h3");
-  //newH3.textContent = `Based on Number in Household and Size of Home`;
-  //newP.textContent = `This number is based on the number of housedhold of ${arr[0]} (score: ${arr[3]})`;
-  //newP.textContent += `and a ${arr[1]} size of home (score: ${arr[2]})`;
-  //output.appendChild(newH2);
-  //output.appendChild(newH3);
-  //output.appendChild(newP);
-  //}
-  //}
-  
-  start(1, "apt");
-  start(2, "apt");
-  start(3, "apt");
-  start(4, "apt");
-  start(5, "apt");
-displayOutput();
+}
+
+displayOutMovies();
