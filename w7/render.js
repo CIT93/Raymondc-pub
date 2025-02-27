16383c65bd9ca46a75151f464afafeb88265e7ad
cheray91 <cheray91@icloud.com>
2025-02-27 23:38:59 +0000

    my attempt at the code, before I review other students code

--- w7/render.js
+++ w7/render.js
@@ -1,38 +1,54 @@
 const TBL = document.getElementById("tab-data");
 
 function renderTblHeading() {
-  const table = document.createElement("table");
-  const thead = document.createElement("thead");
-  const tr = document.createElement("tr");
-  const headingTextArr = [
-    "Name",
-    "Household",
-    "HouseSize",
-    "Footprint",
-    "Actions",
-  ];
-  headingTextArr.forEach(function (text) {
-    const th = document.createElement("th");
-    th.textContent = text;
-    tr.appendChild(th);
-    console.log(tr);
-  });
-  thead.appendChild(tr);
-  table.appendChild(thead);
+  let existingTable = document.getElementById("table-data");
+  if (!existingTable) {
+    const table = document.createElement("table");
+    table.id = "table-data";
 
-  return table;
+    const thead = document.createElement("thead");
+    const tr = document.createElement("tr");
+    const headingTextArr = ["Name", "Total"];
+
+    headingTextArr.forEach(function (text) {
+      const th = document.createElement("th");
+      th.textContent = text;
+      tr.appendChild(th);
+    });
+
+    thead.appendChild(tr);
+    table.appendChild(thead);
+
+    const tbody = document.createElement("tbody");
+    tbody.id = "table-body";
+    table.appendChild(tbody);
+
+    TBL.appendChild(table);
+  }
 }
+
 function renderTbl(data) {
-  const table = renderTblHeading();
-  const tbody = document.createElement("tbody");
-  const tr = document.createElement("tr");
-  const trTextArr = ["Raymond", "3", "large", "20"];
-  trTextArr.forEach(function (text) {
-    const td = document.createElement("td");
-    td.textContent = text;
-    tr.appendChild(td);
+  renderTblHeading();
+
+  const tbody = document.getElementById("table-body");
+
+  tbody.innerHTML = "";
+
+  data.forEach(function (obj) {
+    const tr = document.createElement("tr");
+
+    const tdName = document.createElement("td");
+    const tdTotal = document.createElement("td");
+
+    tdName.textContent = obj.firstName;
+    tdTotal.textContent = obj.cfpTotal;
+
+    tr.appendChild(tdName);
+    tr.appendChild(tdTotal);
+    tbody.appendChild(tr);
   });
-  const td = document.createElement("td");
+}
+/*const td = document.createElement("td");
   const btnEdit = document.createElement("button");
   const btnDel = document.createElement("button");
 
@@ -41,10 +57,7 @@ function renderTbl(data) {
   td.appendChild(btnEdit);
   td.appendChild(btnDel);
   tr.appendChild(td);
-  tbody.appendChild(tr);
-  table.appendChild(tbody);
-  console.log(table);
 
-  TBL.appendChild(table);
-}
+  tbody.appendChild(tr);*/
+
 export { renderTbl, renderTblHeading };
